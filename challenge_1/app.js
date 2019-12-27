11d7c54f8775d3ae13c8096ffc47a25f464d2f10
psktcsharp <safeerlibya@gmail.com>
2019-12-27 19:53:40 +0100

    fixed a bug with a missing winning case - loops refactor

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -33,12 +33,13 @@ class TicGame {
             document.getElementById("gameStateLabel").innerText = "PLAYER O WON !! GG"
             document.getElementById("gameStateLabel").style.background = "gold"
             this.gameEnded()
+            return
         } else if (this.winnerCheck() === false) {
             document.getElementById("gameStateLabel").innerText = "PLAYER X WON !! GG"
             document.getElementById("gameStateLabel").style.background = "gold"
             this.gameEnded()
         }
-        if (this.endGameCheck()) {
+        else if (this.endGameCheck()) {
             document.getElementById("gameStateLabel").innerText = "IT'S A TIE !! GG"
             document.getElementById("gameStateLabel").style.background = "gray"
             this.gameEnded()
@@ -47,15 +48,11 @@ class TicGame {
     //end the game disabling all button
     gameEnded() {
         var rows = document.getElementById("gameTable").rows
-        for (var i = 0; i < 3; i++) {
-            rows[0].cells[i].childNodes[0].disabled = true;
-        }
-        for (var i = 0; i < 3; i++) {
-            rows[1].cells[i].childNodes[0].disabled = true;
-        }
-        for (var i = 0; i < 3; i++) {
-            rows[2].cells[i].childNodes[0].disabled = true;
-        }
+        for(var j=0;j<3;j++){
+            for(var i=0;i<3;i++){
+            rows[j].cells[i].childNodes[0].disabled = true;
+            }
+    }
     }
     //checks if all buttons were clicked, end game if true
     endGameCheck() {
@@ -63,18 +60,13 @@ class TicGame {
         //make a counter that will increase everytime you find a button that has a text
         //if that counter exceeds 9 return true
         var filledCounter = 0;
+        for(var j=0;j<3;j++){
         for (var i = 0; i < 3; i++) {
-            if (rows[0].cells[i].childNodes[0].innerText != "⬛") { filledCounter++; }
+            if (rows[j].cells[i].childNodes[0].innerText != "⬛") { filledCounter++; }
             if (filledCounter === 9) { return true }
         }
-        for (var i = 0; i < 3; i++) {
-            if (rows[1].cells[i].childNodes[0].innerText != "⬛") { filledCounter++; }
-            if (filledCounter === 9) { return true }
-        }
-        for (var i = 0; i < 3; i++) {
-            if (rows[2].cells[i].childNodes[0].innerText != "⬛") { filledCounter++; }
-            if (filledCounter === 9) { return true }
-        }
+
+    }
 
         return false;
     }
@@ -85,14 +77,10 @@ class TicGame {
         //make a counter that will increase everytime you find a button that has a text
         //if that counter exceeds 9 return true
         var resultArray = []
+        for(var j=0;j<3;j++){
         for (var i = 0; i < 3; i++) {
-            resultArray.push(rows[0].cells[i].childNodes[0].innerText)
+            resultArray.push(rows[j].cells[i].childNodes[0].innerText)
         }
-        for (var i = 0; i < 3; i++) {
-            resultArray.push(rows[1].cells[i].childNodes[0].innerText)
-        }
-        for (var i = 0; i < 3; i++) {
-            resultArray.push(rows[2].cells[i].childNodes[0].innerText)
         }
         //FOR X WINNER
         if (resultArray[0] === "X" && resultArray[1] === "X" && resultArray[2] === "X") { return false }
@@ -102,6 +90,7 @@ class TicGame {
         if (resultArray[2] === "X" && resultArray[4] === "X" && resultArray[6] === "X") { return false }
         if (resultArray[0] === "X" && resultArray[3] === "X" && resultArray[6] === "X") { return false }
         if (resultArray[1] === "X" && resultArray[4] === "X" && resultArray[7] === "X") { return false }
+        if (resultArray[2] === "X" && resultArray[5] === "X" && resultArray[8] === "X") { return false }
         //FOR O WINNER
         if (resultArray[0] === "O" && resultArray[1] === "O" && resultArray[2] === "O") { return true }
         if (resultArray[3] === "O" && resultArray[4] === "O" && resultArray[5] === "O") { return true }
@@ -110,6 +99,7 @@ class TicGame {
         if (resultArray[2] === "O" && resultArray[4] === "O" && resultArray[6] === "O") { return true }
         if (resultArray[0] === "O" && resultArray[3] === "O" && resultArray[6] === "O") { return true }
         if (resultArray[1] === "O" && resultArray[4] === "O" && resultArray[7] === "O") { return true }
+        if (resultArray[2] === "O" && resultArray[5] === "O" && resultArray[8] === "O") { return true }
 
 
 
