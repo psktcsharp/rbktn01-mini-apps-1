e5e66ce718ba3b8905406e210dc1cc1ad9370cae
psktcsharp <safeerlibya@gmail.com>
2019-12-27 14:55:49 +0100

    BETA is here | added game ending - winner check

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -13,16 +13,46 @@ class TicGame {
             //based on current player do x or o
             if(this.currentPlayer){
                 document.getElementById(buttonId).innerText = "O"
+                document.getElementById(buttonId).style.background = "Crimson"
                 this.currentPlayer = false;
+                document.getElementById("gameStateLabel").innerHTML = "Player X turn"
+                document.getElementById("gameStateLabel").style.background = "Crimson"
             }
             else{
                 document.getElementById(buttonId).innerText = "X"
+                document.getElementById(buttonId).style.background = "darkOliveGreen"
                 this.currentPlayer = true;
+                document.getElementById("gameStateLabel").innerText = "Player O turn"
+                document.getElementById("gameStateLabel").style.background = "darkOliveGreen"
             }
+            document.getElementById(buttonId).disabled = true;
         }
        
-        console.log("something clicked !",buttonId,this.currentPlayer)
+        //console.log("something clicked !",buttonId,this.currentPlayer)
+        if(this.winnerCheck() === true){
+            document.getElementById("gameStateLabel").innerText = "PLAYER O WON !! GG"
+            document.getElementById("gameStateLabel").style.background = "gold"
+            this.gameEnded()
+        }else if(this.winnerCheck() === false){
+            document.getElementById("gameStateLabel").innerText = "PLAYER X WON !! GG"
+            document.getElementById("gameStateLabel").style.background = "gold"
+            this.gameEnded()
+        }
         console.log(this.endGameCheck())
+        console.log()
+    }
+    //end the game disabling all button
+    gameEnded(){
+        var rows = document.getElementById("gameTable").rows
+        for(var i = 0;i < 3; i++){
+            rows[0].cells[i].childNodes[0].disabled = true;
+        }
+        for(var i = 0;i < 3; i++){
+            rows[1].cells[i].childNodes[0].disabled = true;
+        }
+        for(var i = 0;i < 3; i++){
+            rows[2].cells[i].childNodes[0].disabled = true; 
+        }
     }
     //checks if all buttons were clicked, end game if true
     endGameCheck(){      
@@ -44,6 +74,58 @@ class TicGame {
         }
 
         return false;
+    }
+    //check if someone won, by hard coding all possible win scenarios and check agianst it
+    //to be re written in dynamic manners later on
+    winnerCheck(){
+        var rows = document.getElementById("gameTable").rows
+        //make a counter that will increase everytime you find a button that has a text
+        //if that counter exceeds 9 return true
+       var resultArray = []
+        for(var i = 0;i < 3; i++){
+            resultArray.push(rows[0].cells[i].childNodes[0].innerText)       
+        }
+        for(var i = 0;i < 3; i++){
+            resultArray.push(rows[1].cells[i].childNodes[0].innerText)          
+        }
+        for(var i = 0;i < 3; i++){
+            resultArray.push(rows[2].cells[i].childNodes[0].innerText)       
+        }
+        //FOR X WINNER
+        if(resultArray[0] === "X" &&  resultArray[1] === "X" && resultArray[2] === "X" )
+        {return false}
+        if(resultArray[3] === "X" &&  resultArray[4] === "X" && resultArray[5] === "X" )
+        {return false}
+        if(resultArray[6] === "X" &&  resultArray[7] === "X" && resultArray[8] === "X" )
+        {return false}
+        if(resultArray[0] === "X" &&  resultArray[4] === "X" && resultArray[8] === "X" )
+        {return false}
+        if(resultArray[2] === "X" &&  resultArray[4] === "X" && resultArray[6] === "X" )
+        {return false}
+        if(resultArray[0] === "X" &&  resultArray[3] === "X" && resultArray[6] === "X" )
+        {return false}
+        if(resultArray[1] === "X" &&  resultArray[4] === "X" && resultArray[7] === "X" )
+        {return false}
+        //FOR O WINNER
+        if(resultArray[0] === "O" &&  resultArray[1] === "O" && resultArray[2] === "O" )
+        {return true}
+        if(resultArray[3] === "O" &&  resultArray[4] === "O" && resultArray[5] === "O" )
+        {return true}
+        if(resultArray[6] === "O" &&  resultArray[7] === "O" && resultArray[8] === "O" )
+        {return true}
+        if(resultArray[0] === "O" &&  resultArray[4] === "O" && resultArray[8] === "O" )
+        {return true}
+        if(resultArray[2] === "O" &&  resultArray[4] === "O" && resultArray[6] === "O" )
+        {return true}
+        if(resultArray[0] === "O" &&  resultArray[3] === "O" && resultArray[6] === "O" )
+        {return true}
+        if(resultArray[1] === "O" &&  resultArray[4] === "O" && resultArray[7] === "O" )
+        {return true}
+
+               
+        
+        console.log(resultArray)
+        return  null;
     }
   }
   var newGame = new TicGame();
