3f7a70eed036ed25441fbcae0d87545596c04931
psktcsharp <safeerlibya@gmail.com>
2019-12-28 14:35:31 +0100

    rendering the first object as a response

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path');
+
+
 //handle get on root dir
 app.get('/', (req, res) => res.send('Hello World!'))
 //handle incoming post /json
-app.post('/json',function(req,res){
-   
-    console.log(req.body);
-    res.end("yes");
+app.get('/json',function(req,res){
+   //convert to json
+    var jsonObj = JSON.parse(req.query.jsonText);
+    //create an html var to render cvs report for the client
+    var htmlToRender = "<table>"
+    for(var key in jsonObj){
+        htmlToRender += "<tr><td>"
+        htmlToRender += jsonObj[key]
+        htmlToRender += "</td></tr>"
+    }
+    htmlToRender += "</table>"
+    res.end(`${htmlToRender}`);
   });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
