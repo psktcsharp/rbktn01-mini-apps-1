bf287e992e755413e71dd71f7e0c28d2fb0032ba
psktcsharp <safeerlibya@gmail.com>
2019-12-28 11:42:32 +0100

    add package | worked on client to send post using form

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,19 @@
+//a class to handle client work
+class Client{
+    constructor(){
+
+    }
+    //a method to send json data entred in text area to the server as a post
+    submit(){  
+
+        
+        // var post = $.post('http://localhost:3000/json',{jsonData:"jquery here",dataType:'jsonp'})
+        // post.done(function(data){
+        //     var content = $(data).find("#textArea");
+        //    console.log(content)
+        // })
+    }
+}
+var aClient = new Client();
 
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+//handle get on root dir
+app.get('/', (req, res) => res.send('Hello World!'))
+//handle incoming post /json
+app.post('/json',function(req,res){
+   
+    console.log(req.body);
+    res.end("yes");
+  });
 
-
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
