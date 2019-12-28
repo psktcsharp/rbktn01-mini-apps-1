ef8eb6d032fc51863230f5d1ac4f3e137d94f8f9
psktcsharp <safeerlibya@gmail.com>
2019-12-28 16:57:05 +0100

    checkpoint

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -19,7 +19,7 @@ app.get('/json',function(req,res){
     }
     htmlToRender += "<tr>"
  // render html for a child and add it to table
-
+    
 
      renderHTML = (aChild) => {
          console.log("has a kid ? ",aChild)
@@ -29,15 +29,45 @@ app.get('/json',function(req,res){
             if(jsonObj["children"].length >0)
             {
                 console.log("found kids trying to render")
+               
                 for(var i = 0 ;i< jsonObj["children"].length;i++){
-                    kids.push(jsonObj["children"][i])
+                   if(!aChild){
+                      
+                    extractKids(jsonObj["children"][i])
+                   }                
                 }
+                      
             }
             else{}
         }
     }
-    //clean dublicated entries
+    //extract children outside to avoid double pushing
+    var cleanKids = []
+    for(var i = 0;i< kids.length ;i++){
+        var doubleCounter = 0;
+        for(var j=0; j<kids.length;j++){
+            if(deepEquals(kids[i],kids[j])){
+                doubleCounter++;
+            }
+            
+        }
+        if(doubleCounter < 2){
+            cleanKids.push[kids[i]]
+        }
+    }
+     extractKids = (aKid) =>{
+        
+        kids.push(aKid)
+        if(aKid['children']){
+            for(var i = 0 ;i< aKid['children'].length;i++){
+            extractKids(aKid['children'][i])
+            }
+        }
+    }
+  
     renderHTML()
+      //clean dublicated entries
+      
     if(kids.length > 0){
         for(var i = 0; i < kids.length;i++){
         htmlToRender += "<tr>"
@@ -49,5 +79,20 @@ app.get('/json',function(req,res){
       
     res.end(`${htmlToRender}`);
   });
-  
+  // comparing objects to avoid the dublicated entry bug on csv report view
+  var deepEquals = function(obj1, obj2) {
+
+    if (obj1 === obj2) { return true; }
+    if (obj1 && !obj2 || !obj1 && obj2) { return false; }
+    if (!(obj1 instanceof Object) || !(obj2 instanceof Object)) { return false; }
+    var obj1Keys = Object.keys(obj1);
+    var obj2Keys = Object.keys(obj2);
+    if (obj1Keys.length !== obj2Keys.length) { return false; }
+    if (obj1Keys.length === 0) { return true; } 
+    for (var i = 0; i < obj1Keys.length; i++) {
+      if (!obj2.hasOwnProperty(obj1Keys[i])) { return false; }
+      if (!deepEquals(obj1[obj1Keys[i]], obj2[obj1Keys[i]])) { return false; }
+    }
+    return true;
+    };
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
